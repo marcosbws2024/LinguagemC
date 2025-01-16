0f14388c8896471f894f66f5ba8aa7f622a53863
marcosbws2024 <marcosbws2024@gmail.com>
2025-01-15 22:25:34 -0300

    .

--- Operadores/preposincremento.c
+++ Operadores/preposincremento.c
@@ -14,11 +14,18 @@ int main()
     int resultado =0;
 
     printf("Antes Incremento:%d\n",a);
+
     resultado = a++;
     printf("Apos o  Pos-Incremento- num1: %d - Resultado:%d\n",a,resultado);
 
     resultado = ++a;
     printf("Apos o  Pre-Incremento- num1: %d - Resultado:%d\n",a,resultado);
+
+    resultado = a--;
+    printf("Apos o  Pos-decremento- num1: %d - Resultado:%d\n",a,resultado);
+
+    resultado = --a;
+    printf("Apos o  Pre-decremento- num1: %d - Resultado:%d\n",a,resultado);
     
 
    return 0;
